a0b5b00c03758bf267025d8b7d7b991b2a4b94e7
danieldorazio <danieldorazio.97@gmail.com>
2023-11-22 14:10:54 +0100

    add: AppTitle in components and imported in App.vue

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<script>
+    export default {
+        data () {
+            return {
+                text: "La mia prima app con Vite!",
+            };
+        },
+    };
+
+</script>
+
+<template>
+    <h1>{{ text }}</h1>
+</template>
+
+<style>
+
+</style>
